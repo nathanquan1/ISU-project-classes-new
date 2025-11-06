e7e5af66059b5facb2674d5e8701df176796db08
Anze Yuan <Adam.az.yuan@gmail.com>
2025-11-05 20:40:49 -0500

    Title pages + Make UI better

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -7,9 +7,16 @@ public class SceneSwitcher : MonoBehaviour
 {
     // Start is called before the first frame update
     public Gameplay gameplay;
+
+    private static bool _bootedToMain = false;
+
     void Start()
     {
-        
+        if (!_bootedToMain && SceneManager.GetActiveScene().name != "MainScene")
+        {
+            _bootedToMain = true;
+            SceneManager.LoadScene("MainScene");
+        }
     }
 
     // Update is called once per frame
@@ -19,6 +26,7 @@ public class SceneSwitcher : MonoBehaviour
     }
     public void PlayGame()
     {
+        _bootedToMain = true;
         SceneManager.LoadScene("GameScene");
         //variable : public Gameplay gameplay;
         //gameplay.StartGame();
@@ -26,6 +34,7 @@ public class SceneSwitcher : MonoBehaviour
     
     public void Homescreen()
     {
+        _bootedToMain = true;
         SceneManager.LoadScene("MainScene");
     }
 }
