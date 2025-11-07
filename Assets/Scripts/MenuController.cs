b124948a142b04fc25464e2624df576b28e42398
FearlessYak5088 <sunfan2266@gmail.com>
2025-11-07 14:15:44 -0500

    Created Volume Slider and BG Music

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     public GameObject playMenu;
     public GameObject settingsMenu;
     public GameObject loadoutMenu;
+    public AudioClip MainMenuTheme;
+    public AudioSource sound;
+    public AudioSource music;
 
     //public Gameplay gameplay;
     void Start()
     {
         OpenPlayMenu();
+
+        music.PlayOneShot(MainMenuTheme); // Music looped
+        music.volume = 0.0f;
+        sound.volume = 0.0f;
     }
 
     // Update is called once per frame
@@ -40,5 +49,12 @@ public class MenuController : MonoBehaviour
 
         loadoutMenu.SetActive(true);
     }
+
+    public void SetVolume(float value)
+    {
+        music.volume = value;
+        sound.volume = value;
+    }
+
 
 }
---
+++ Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class SettingsMenu : MonoBehaviour
+{
+    public GameObject SettingsPanel; 
+    public Slider Volume;
+    public MenuController MenuController; 
+    public TextMeshProUGUI VolumeText;
+
+
+    void Start()
+    {
+        //Sets volume to the slider volume level by calling the setVolume function in cookie_script
+        Volume.onValueChanged.AddListener(MenuController.SetVolume);
+        VolumeText.text = "Volume: " + Mathf.RoundToInt(MenuController.music.volume*100).ToString()+"%";
+    }
+    void Update()
+    {
+        VolumeText.text = "Volume: " + Mathf.RoundToInt(MenuController.music.volume*100).ToString()+"%";
+    }
+
+    public void ToggleSettings()
+    {
+        //Toggles settings panel on/off when clicked
+        SettingsPanel.SetActive(!SettingsPanel.activeSelf); 
+
+    }
+}
