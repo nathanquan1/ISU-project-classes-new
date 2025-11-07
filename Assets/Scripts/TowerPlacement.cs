bea66cd3d8c84c9b922e7823607708754c63dc9a
nathanquan1 <nathan.quan26@ycdsbk12.ca>
2025-11-06 21:11:14 -0500

    Added tower selecting

--- Assets/Scripts/TowerPlacement.cs
+++ Assets/Scripts/TowerPlacement.cs
@@ -5,11 +5,32 @@ using UnityEngine;
 public class TowerPlacement : MonoBehaviour
 {
     public Gameplay gameplay;
+    public GameObject towerPreview;
+    public TowerController towerController;
     private bool _selected = false;
-    public void buttonSelected()
+    public void togglePlacing()
     {
         _selected = !_selected;
-        
+    }
+    public void Start()
+    {
+        towerPreview.SetActive(false);
+    }
+    public void Update()
+    {
+        if (_selected)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            towerPreview.transform.position = mousePos;
+
+            towerPreview.SetActive(true);
+
+            
+        }
+        else
+        {
+            towerPreview.SetActive(false);
+        }
     }
 
 }
