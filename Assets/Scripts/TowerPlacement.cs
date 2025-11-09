af9835b65408e6de306848670cdff50f7bb9a4d9
nathanquan1 <nathan.quan26@ycdsbk12.ca>
2025-11-09 11:20:57 -0500

    Added colliders to path so you cant place on path

--- Assets/Scripts/TowerPlacement.cs
+++ Assets/Scripts/TowerPlacement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -13,6 +14,8 @@ public class TowerPlacement : MonoBehaviour
     public CannonTowerController cannonTowerController;
     public MinigunTowerController minigunTowerController;
     public MissileTowerController missileTowerController;
+    protected RaycastHit2D hit;
+    protected bool touchingPath;
 
     //public bool TouchingPath;
     protected bool _selected = false;
@@ -27,16 +30,23 @@ public class TowerPlacement : MonoBehaviour
     {
         towerPreview.SetActive(false);
         pathway.SetActive(false);
+        touchingPath = false;
     }
     protected virtual void Update()
     {
         towerPreview.SetActive(_selected); //will only show if selected
         pathway.SetActive(_selected);
+
+        hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero); //is set to what mouse is touching, vector2.zero means no direction since 2d
+        //bool touchingPathComponents = hit.collider.transform.IsChildOf(pathway.transform);
+        touchingPath =  hit.collider != null && hit.collider.transform.IsChildOf(pathway.transform); //mouse touching path bool variable 
+        Debug.Log(touchingPath);//ischildof checks if it is touching the pathway components since it is broken up into multiple gameobjects
+
         if (_selected)
         {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            towerPreview.transform.position = mousePos;
-            if (Input.GetMouseButton(0)) //click
+            towerPreview.transform.position = mousePos;//moves the preview image to your mouse
+            if (Input.GetMouseButton(0) &&!touchingPath) //click
             {
 
                 if (gameplay.GetMoney() >= Price)
